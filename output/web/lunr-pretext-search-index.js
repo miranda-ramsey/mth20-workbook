1de4aadb4bdd1b202480ed93d16e75550ffa1f92
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2025-03-11 19:19:14 -0700

    basic structure

---
+++ output/web/lunr-pretext-search-index.js
@@ -0,0 +1,356 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Miranda Ramsey Math Department Portland Community College      My Website   copyright         "
+},
+{
+  "id": "ws-introduction",
+  "level": "1",
+  "url": "ws-introduction.html",
+  "type": "Subsection",
+  "number": "",
+  "title": "Worksheet: Introduction",
+  "body": " Worksheet: Introduction  This section is just a wrapper for the actual worksheet.   Preparation     Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:      What do the following limits evaluate to?                                                                                        Evaluate the following limits involving composite functions.                                      Evaluate the following limits involving composite functions.                                    "
+},
+{
+  "id": "prep-class-introduction-2-1-2",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-1-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": " Write down the room number and meeting time for our class:  "
+},
+{
+  "id": "prep-class-introduction-2-1-3",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-1-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": " Write down the instructor's email address:  "
+},
+{
+  "id": "prep-class-introduction-2-2-2",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-2",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-3",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-3",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-4",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-4",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-5",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-5",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-6",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-6",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-7",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-7",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-8",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-8",
+  "type": "Worksheet Exercise",
+  "number": "9",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-9",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-9",
+  "type": "Worksheet Exercise",
+  "number": "10",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-10",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-10",
+  "type": "Worksheet Exercise",
+  "number": "11",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-11",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-11",
+  "type": "Worksheet Exercise",
+  "number": "12",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-12",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-12",
+  "type": "Worksheet Exercise",
+  "number": "13",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-13",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-13",
+  "type": "Worksheet Exercise",
+  "number": "14",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-14",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-14",
+  "type": "Worksheet Exercise",
+  "number": "15",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-15",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-15",
+  "type": "Worksheet Exercise",
+  "number": "16",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-16",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-16",
+  "type": "Worksheet Exercise",
+  "number": "17",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-2-2-17",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-2-2-17",
+  "type": "Worksheet Exercise",
+  "number": "18",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-3-1-2",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-3-1-2",
+  "type": "Worksheet Exercise",
+  "number": "19",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-3-1-3",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-3-1-3",
+  "type": "Worksheet Exercise",
+  "number": "20",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-3-1-4",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-3-1-4",
+  "type": "Worksheet Exercise",
+  "number": "21",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-3-1-5",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-3-1-5",
+  "type": "Worksheet Exercise",
+  "number": "22",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-3-1-6",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-3-1-6",
+  "type": "Worksheet Exercise",
+  "number": "23",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-3-1-7",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-3-1-7",
+  "type": "Worksheet Exercise",
+  "number": "24",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-4-1-2",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-4-1-2",
+  "type": "Worksheet Exercise",
+  "number": "25",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-4-1-3",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-4-1-3",
+  "type": "Worksheet Exercise",
+  "number": "26",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-4-1-4",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-4-1-4",
+  "type": "Worksheet Exercise",
+  "number": "27",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-4-1-5",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-4-1-5",
+  "type": "Worksheet Exercise",
+  "number": "28",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-4-1-6",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-4-1-6",
+  "type": "Worksheet Exercise",
+  "number": "29",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "prep-class-introduction-4-1-7",
+  "level": "2",
+  "url": "ws-introduction.html#prep-class-introduction-4-1-7",
+  "type": "Worksheet Exercise",
+  "number": "30",
+  "title": "",
+  "body": "    "
+},
+{
+  "id": "ws-whole-numbers",
+  "level": "1",
+  "url": "ws-whole-numbers.html",
+  "type": "Subsection",
+  "number": "",
+  "title": "Worksheet: Whole Numbers",
+  "body": " Worksheet: Whole Numbers   In this section...    Textbook Reference    Preparation Exercises   Preparation 1  Many of us...    "
+},
+{
+  "id": "fractions",
+  "level": "1",
+  "url": "fractions.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Fractions",
+  "body": " Fractions   "
+},
+{
+  "id": "midterm-review",
+  "level": "1",
+  "url": "midterm-review.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Midterm Review",
+  "body": " Midterm Review   "
+},
+{
+  "id": "rational-numbers",
+  "level": "1",
+  "url": "rational-numbers.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Rational Numbers",
+  "body": " Rational Numbers   "
+},
+{
+  "id": "final-review",
+  "level": "1",
+  "url": "final-review.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Final Review",
+  "body": " Final Review   "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
