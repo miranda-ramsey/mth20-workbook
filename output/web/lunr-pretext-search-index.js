2bde7d449eb160b495adf6b8855f282c0472cb2e
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2025-03-25 15:29:18 -0700

    arithmetic worksheet update

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -15,278 +15,62 @@ var ptx_lunr_docs = [
   "url": "ws-introduction.html",
   "type": "Subsection",
   "number": "",
-  "title": "Worksheet: Introduction",
-  "body": " Worksheet: Introduction  This section is just a wrapper for the actual worksheet.   Preparation     Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:      What do the following limits evaluate to?                                                                                        Evaluate the following limits involving composite functions.                                      Evaluate the following limits involving composite functions.                                    "
+  "title": "Introduction",
+  "body": " Introduction   This section is just a wrapper for the actual worksheet.    Worksheet: Introduction    Preparation   Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:    What are three ways you can obtain help in this class?       Preparation   Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:    What are three ways you can obtain help in this class?      "
 },
 {
-  "id": "prep-class-introduction-2-1-2",
+  "id": "worksheet-introduction-2-1-3",
   "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-1-2",
+  "url": "ws-introduction.html#worksheet-introduction-2-1-3",
   "type": "Worksheet Exercise",
   "number": "1",
   "title": "",
   "body": " Write down the room number and meeting time for our class:  "
 },
 {
-  "id": "prep-class-introduction-2-1-3",
+  "id": "worksheet-introduction-2-1-4",
   "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-1-3",
+  "url": "ws-introduction.html#worksheet-introduction-2-1-4",
   "type": "Worksheet Exercise",
   "number": "2",
   "title": "",
   "body": " Write down the instructor's email address:  "
 },
 {
-  "id": "prep-class-introduction-2-2-2",
+  "id": "worksheet-introduction-2-1-5",
   "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-2",
+  "url": "ws-introduction.html#worksheet-introduction-2-1-5",
   "type": "Worksheet Exercise",
   "number": "3",
   "title": "",
-  "body": "    "
+  "body": " What are three ways you can obtain help in this class?  "
 },
 {
-  "id": "prep-class-introduction-2-2-3",
+  "id": "worksheet-introduction-3-1-3",
   "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-3",
+  "url": "ws-introduction.html#worksheet-introduction-3-1-3",
   "type": "Worksheet Exercise",
   "number": "4",
   "title": "",
-  "body": "    "
+  "body": " Write down the room number and meeting time for our class:  "
 },
 {
-  "id": "prep-class-introduction-2-2-4",
+  "id": "worksheet-introduction-3-1-4",
   "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-4",
+  "url": "ws-introduction.html#worksheet-introduction-3-1-4",
   "type": "Worksheet Exercise",
   "number": "5",
   "title": "",
-  "body": "    "
+  "body": " Write down the instructor's email address:  "
 },
 {
-  "id": "prep-class-introduction-2-2-5",
+  "id": "worksheet-introduction-3-1-5",
   "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-5",
+  "url": "ws-introduction.html#worksheet-introduction-3-1-5",
   "type": "Worksheet Exercise",
   "number": "6",
   "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-6",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-6",
-  "type": "Worksheet Exercise",
-  "number": "7",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-7",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-7",
-  "type": "Worksheet Exercise",
-  "number": "8",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-8",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-8",
-  "type": "Worksheet Exercise",
-  "number": "9",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-9",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-9",
-  "type": "Worksheet Exercise",
-  "number": "10",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-10",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-10",
-  "type": "Worksheet Exercise",
-  "number": "11",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-11",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-11",
-  "type": "Worksheet Exercise",
-  "number": "12",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-12",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-12",
-  "type": "Worksheet Exercise",
-  "number": "13",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-13",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-13",
-  "type": "Worksheet Exercise",
-  "number": "14",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-14",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-14",
-  "type": "Worksheet Exercise",
-  "number": "15",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-15",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-15",
-  "type": "Worksheet Exercise",
-  "number": "16",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-16",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-16",
-  "type": "Worksheet Exercise",
-  "number": "17",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-2-2-17",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-2-2-17",
-  "type": "Worksheet Exercise",
-  "number": "18",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-3-1-2",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-3-1-2",
-  "type": "Worksheet Exercise",
-  "number": "19",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-3-1-3",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-3-1-3",
-  "type": "Worksheet Exercise",
-  "number": "20",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-3-1-4",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-3-1-4",
-  "type": "Worksheet Exercise",
-  "number": "21",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-3-1-5",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-3-1-5",
-  "type": "Worksheet Exercise",
-  "number": "22",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-3-1-6",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-3-1-6",
-  "type": "Worksheet Exercise",
-  "number": "23",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-3-1-7",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-3-1-7",
-  "type": "Worksheet Exercise",
-  "number": "24",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-4-1-2",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-4-1-2",
-  "type": "Worksheet Exercise",
-  "number": "25",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-4-1-3",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-4-1-3",
-  "type": "Worksheet Exercise",
-  "number": "26",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-4-1-4",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-4-1-4",
-  "type": "Worksheet Exercise",
-  "number": "27",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-4-1-5",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-4-1-5",
-  "type": "Worksheet Exercise",
-  "number": "28",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-4-1-6",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-4-1-6",
-  "type": "Worksheet Exercise",
-  "number": "29",
-  "title": "",
-  "body": "    "
-},
-{
-  "id": "prep-class-introduction-4-1-7",
-  "level": "2",
-  "url": "ws-introduction.html#prep-class-introduction-4-1-7",
-  "type": "Worksheet Exercise",
-  "number": "30",
-  "title": "",
-  "body": "    "
+  "body": " What are three ways you can obtain help in this class?  "
 },
 {
   "id": "ws-whole-numbers",
@@ -294,8 +78,224 @@ var ptx_lunr_docs = [
   "url": "ws-whole-numbers.html",
   "type": "Subsection",
   "number": "",
-  "title": "Worksheet: Whole Numbers",
-  "body": " Worksheet: Whole Numbers   In this section...    Textbook Reference    Preparation Exercises   Preparation 1  Many of us...    "
+  "title": "Whole Numbers",
+  "body": " Whole Numbers   In this section...    Textbook Reference    Worksheet: Arithmetic Practice    Practice with Units.   Perform the indicated operation and write your answer with the proper units.                            Preparation   Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:    What are three ways you can obtain help in this class?      "
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-3",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-4",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-5",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-5",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-6",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-6",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-7",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-7",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-8",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-8",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-9",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-9",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-10",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-10",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-11",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-11",
+  "type": "Worksheet Exercise",
+  "number": "9",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-12",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-12",
+  "type": "Worksheet Exercise",
+  "number": "10",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-13",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-13",
+  "type": "Worksheet Exercise",
+  "number": "11",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-14",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-14",
+  "type": "Worksheet Exercise",
+  "number": "12",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-15",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-15",
+  "type": "Worksheet Exercise",
+  "number": "13",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-16",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-16",
+  "type": "Worksheet Exercise",
+  "number": "14",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-17",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-17",
+  "type": "Worksheet Exercise",
+  "number": "15",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-18",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-18",
+  "type": "Worksheet Exercise",
+  "number": "16",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-19",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-19",
+  "type": "Worksheet Exercise",
+  "number": "17",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-20",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-20",
+  "type": "Worksheet Exercise",
+  "number": "18",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-21",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-21",
+  "type": "Worksheet Exercise",
+  "number": "19",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-22",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-22",
+  "type": "Worksheet Exercise",
+  "number": "20",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-2-1-23",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-2-1-23",
+  "type": "Worksheet Exercise",
+  "number": "21",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "worksheet-arithmetic-practice-3-1-3",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-1-3",
+  "type": "Worksheet Exercise",
+  "number": "22",
+  "title": "",
+  "body": " Write down the room number and meeting time for our class:  "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-1-4",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-1-4",
+  "type": "Worksheet Exercise",
+  "number": "23",
+  "title": "",
+  "body": " Write down the instructor's email address:  "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-1-5",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-1-5",
+  "type": "Worksheet Exercise",
+  "number": "24",
+  "title": "",
+  "body": " What are three ways you can obtain help in this class?  "
 },
 {
   "id": "fractions",
