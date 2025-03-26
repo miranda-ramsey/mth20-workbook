483a0953b66369fe859f4c7e9497b9be15ced0eb
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2025-03-25 21:23:34 -0700

    added word problems to arithmetic practice

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Subsection",
   "number": "",
   "title": "Introduction",
-  "body": " Introduction   This section is just a wrapper for the actual worksheet.    Worksheet: Introduction    Preparation   Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:    What are three ways you can obtain help in this class?       Preparation   Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:    What are three ways you can obtain help in this class?      "
+  "body": " Introduction   This section is just a wrapper for the actual worksheet.    Worksheet: Introduction    Preparation   Answer the following questions about the syllabus.     Write down the room number and meeting time for our class:      Write down the instructor's email address:      What are three ways you can obtain help in this class?        Preparation   Answer the following questions about the syllabus.     Write down the room number and meeting time for our class:      Write down the instructor's email address:      What are three ways you can obtain help in this class?       "
 },
 {
   "id": "worksheet-introduction-2-1-3",
@@ -25,7 +25,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "1",
   "title": "",
-  "body": " Write down the room number and meeting time for our class:  "
+  "body": "  Write down the room number and meeting time for our class:   "
 },
 {
   "id": "worksheet-introduction-2-1-4",
@@ -34,7 +34,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "2",
   "title": "",
-  "body": " Write down the instructor's email address:  "
+  "body": "  Write down the instructor's email address:   "
 },
 {
   "id": "worksheet-introduction-2-1-5",
@@ -43,7 +43,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "3",
   "title": "",
-  "body": " What are three ways you can obtain help in this class?  "
+  "body": "  What are three ways you can obtain help in this class?   "
 },
 {
   "id": "worksheet-introduction-3-1-3",
@@ -52,7 +52,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "4",
   "title": "",
-  "body": " Write down the room number and meeting time for our class:  "
+  "body": "  Write down the room number and meeting time for our class:   "
 },
 {
   "id": "worksheet-introduction-3-1-4",
@@ -61,7 +61,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "5",
   "title": "",
-  "body": " Write down the instructor's email address:  "
+  "body": "  Write down the instructor's email address:   "
 },
 {
   "id": "worksheet-introduction-3-1-5",
@@ -70,7 +70,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "6",
   "title": "",
-  "body": " What are three ways you can obtain help in this class?  "
+  "body": "  What are three ways you can obtain help in this class?   "
 },
 {
   "id": "ws-whole-numbers",
@@ -79,7 +79,7 @@ var ptx_lunr_docs = [
   "type": "Subsection",
   "number": "",
   "title": "Whole Numbers",
-  "body": " Whole Numbers   In this section...    Textbook Reference    Worksheet: Arithmetic Practice    Practice with Units.   Perform the indicated operation and write your answer with the proper units.                            Preparation   Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:    What are three ways you can obtain help in this class?      "
+  "body": " Whole Numbers   In this section...    Worksheet: Arithmetic Practice    Practice with Units.   Perform the indicated operation and write your answer with the proper units.                                                                                                                Problem Solving with Whole Numbers   For each problem, decide which mathematical operation(s) is appropriate to use and then find the answer. Show your thinking with words, symbols, and\/or pictures. Write your answer in a complete sentence.      A movie theater makes a profit on each ticket sold. How many tickets must be sold to make a profit of ?  Mathematical operation(s):  Solution:        A savings account contained . After a withdrawal of and a deposit of , how much is now in the account?  Mathematical operation(s):  Solution:        How many tablets should a pharmacist give a person who needs to take tablets times a day for days?  Mathematical operation(s):  Solution:        The cost of a student parking pass at PCC is per term if you buy it online. If a daily pass is , after how many days does a term pass pay off?  Mathematical operation(s):  Solution:       Word Association   What operation is associated with each of the following words?    total    shared equally    double    loss    goes into    triple    reduced by    altogether      "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-3",
@@ -88,7 +88,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "1",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-4",
@@ -97,7 +97,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "2",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-5",
@@ -106,7 +106,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "3",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-6",
@@ -115,7 +115,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "4",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-7",
@@ -124,7 +124,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "5",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-8",
@@ -133,7 +133,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "6",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-9",
@@ -142,7 +142,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "7",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-10",
@@ -151,7 +151,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "8",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-11",
@@ -160,7 +160,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "9",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-12",
@@ -169,7 +169,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "10",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-13",
@@ -178,7 +178,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "11",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-14",
@@ -187,7 +187,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "12",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-15",
@@ -196,7 +196,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "13",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-16",
@@ -205,7 +205,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "14",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-17",
@@ -214,7 +214,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "15",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-18",
@@ -223,7 +223,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "16",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-19",
@@ -232,7 +232,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "17",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-20",
@@ -241,7 +241,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "18",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-21",
@@ -250,7 +250,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "19",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-22",
@@ -259,7 +259,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "20",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-2-1-23",
@@ -268,7 +268,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "21",
   "title": "",
-  "body": ""
+  "body": "    "
 },
 {
   "id": "worksheet-arithmetic-practice-3-1-3",
@@ -277,7 +277,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "22",
   "title": "",
-  "body": " Write down the room number and meeting time for our class:  "
+  "body": "   A movie theater makes a profit on each ticket sold. How many tickets must be sold to make a profit of ?  Mathematical operation(s):  Solution:    "
 },
 {
   "id": "worksheet-arithmetic-practice-3-1-4",
@@ -286,7 +286,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "23",
   "title": "",
-  "body": " Write down the instructor's email address:  "
+  "body": "   A savings account contained . After a withdrawal of and a deposit of , how much is now in the account?  Mathematical operation(s):  Solution:    "
 },
 {
   "id": "worksheet-arithmetic-practice-3-1-5",
@@ -295,7 +295,88 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "24",
   "title": "",
-  "body": " What are three ways you can obtain help in this class?  "
+  "body": "   How many tablets should a pharmacist give a person who needs to take tablets times a day for days?  Mathematical operation(s):  Solution:    "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-1-6",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-1-6",
+  "type": "Worksheet Exercise",
+  "number": "25",
+  "title": "",
+  "body": "   The cost of a student parking pass at PCC is per term if you buy it online. If a daily pass is , after how many days does a term pass pay off?  Mathematical operation(s):  Solution:    "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-2-3",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-2-3",
+  "type": "Worksheet Exercise",
+  "number": "26",
+  "title": "",
+  "body": " total  "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-2-4",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-2-4",
+  "type": "Worksheet Exercise",
+  "number": "27",
+  "title": "",
+  "body": " shared equally  "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-2-5",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-2-5",
+  "type": "Worksheet Exercise",
+  "number": "28",
+  "title": "",
+  "body": " double  "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-2-6",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-2-6",
+  "type": "Worksheet Exercise",
+  "number": "29",
+  "title": "",
+  "body": " loss  "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-2-7",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-2-7",
+  "type": "Worksheet Exercise",
+  "number": "30",
+  "title": "",
+  "body": " goes into  "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-2-8",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-2-8",
+  "type": "Worksheet Exercise",
+  "number": "31",
+  "title": "",
+  "body": " triple  "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-2-9",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-2-9",
+  "type": "Worksheet Exercise",
+  "number": "32",
+  "title": "",
+  "body": " reduced by  "
+},
+{
+  "id": "worksheet-arithmetic-practice-3-2-10",
+  "level": "2",
+  "url": "ws-whole-numbers.html#worksheet-arithmetic-practice-3-2-10",
+  "type": "Worksheet Exercise",
+  "number": "33",
+  "title": "",
+  "body": " altogether  "
 },
 {
   "id": "fractions",
