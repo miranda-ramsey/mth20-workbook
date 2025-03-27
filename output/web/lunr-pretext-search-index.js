7de11afc4009349ead99c5d7e86919b4a46cc74c
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2025-03-26 18:13:18 -0700

    add: first week activies and assignments

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Subsection",
   "number": "",
   "title": "Introduction",
-  "body": " Introduction   This section is just a wrapper for the actual worksheet.    Worksheet: Introduction    Preparation   Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:    What are three ways you can obtain help in this class?       Preparation   Answer the following questions about the syllabus.    Write down the room number and meeting time for our class:    Write down the instructor's email address:    What are three ways you can obtain help in this class?      "
+  "body": " Introduction   Worksheet: Class Information    Syllabus   Answer the following questions about the syllabus.   Write down the room number and meeting time for our class:  Write down the instructor's email address:  On what day and time will the Midterm take place?  On what day and time will the Final take place?  List three ways you can obtain help in this class.  Write down a day and time that you plan to work on your first homework assignment this week.   ALEKS Write down the ALEKS code here.  Group Introductions Write down the names of the people in your group. Make sure to check your spelling!    "
 },
 {
   "id": "worksheet-introduction-2-1-3",
@@ -25,7 +25,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "1",
   "title": "",
-  "body": " Write down the room number and meeting time for our class:  "
+  "body": "Write down the room number and meeting time for our class: "
 },
 {
   "id": "worksheet-introduction-2-1-4",
@@ -34,7 +34,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "2",
   "title": "",
-  "body": " Write down the instructor's email address:  "
+  "body": "Write down the instructor's email address: "
 },
 {
   "id": "worksheet-introduction-2-1-5",
@@ -43,34 +43,52 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "3",
   "title": "",
-  "body": " What are three ways you can obtain help in this class?  "
+  "body": "On what day and time will the Midterm take place? "
 },
 {
-  "id": "worksheet-introduction-3-1-3",
+  "id": "worksheet-introduction-2-1-6",
   "level": "2",
-  "url": "ws-introduction.html#worksheet-introduction-3-1-3",
+  "url": "ws-introduction.html#worksheet-introduction-2-1-6",
   "type": "Worksheet Exercise",
   "number": "4",
   "title": "",
-  "body": " Write down the room number and meeting time for our class:  "
+  "body": "On what day and time will the Final take place? "
 },
 {
-  "id": "worksheet-introduction-3-1-4",
+  "id": "worksheet-introduction-2-1-7",
   "level": "2",
-  "url": "ws-introduction.html#worksheet-introduction-3-1-4",
+  "url": "ws-introduction.html#worksheet-introduction-2-1-7",
   "type": "Worksheet Exercise",
   "number": "5",
   "title": "",
-  "body": " Write down the instructor's email address:  "
+  "body": "List three ways you can obtain help in this class. "
 },
 {
-  "id": "worksheet-introduction-3-1-5",
+  "id": "worksheet-introduction-2-1-8",
   "level": "2",
-  "url": "ws-introduction.html#worksheet-introduction-3-1-5",
+  "url": "ws-introduction.html#worksheet-introduction-2-1-8",
   "type": "Worksheet Exercise",
   "number": "6",
   "title": "",
-  "body": " What are three ways you can obtain help in this class?  "
+  "body": "Write down a day and time that you plan to work on your first homework assignment this week. "
+},
+{
+  "id": "worksheet-introduction-2-2",
+  "level": "2",
+  "url": "ws-introduction.html#worksheet-introduction-2-2",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "ALEKS.",
+  "body": "ALEKS Write down the ALEKS code here. "
+},
+{
+  "id": "worksheet-introduction-2-3",
+  "level": "2",
+  "url": "ws-introduction.html#worksheet-introduction-2-3",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "Group Introductions.",
+  "body": "Group Introductions Write down the names of the people in your group. Make sure to check your spelling! "
 },
 {
   "id": "ws-whole-numbers",
@@ -79,7 +97,7 @@ var ptx_lunr_docs = [
   "type": "Subsection",
   "number": "",
   "title": "Whole Numbers",
-  "body": " Whole Numbers   In this section...    Worksheet: Arithmetic Practice    Practice arithmetic in a handful of different forms  Communicate your process and conclusion clearly and with appropriate units  Identify confusing symbols or operations so you know what to review      Practice with Units   Perform the indicated operation and write your answer with the proper units.                        Word Association   What operation is associated with each of the following words?   total  shared equally  double  loss  goes into  triple  reduced by  altogether    Problem Solving with Whole Numbers   For each problem, decide which mathematical operation(s) is appropriate to use and then find the answer. Show your thinking with words, symbols, and\/or pictures. Write your answer in a complete sentence.   A movie theater makes a profit on each ticket sold. How many tickets must be sold to make a profit of ? Mathematical operation(s): Solution:  A savings account contained . After a withdrawal of and a deposit of , how much is now in the account? Mathematical operation(s): Solution:  How many tablets should a pharmacist give a person who needs to take tablets times a day for days? Mathematical operation(s): Solution:  The cost of a student parking pass at PCC is per term if you buy it online. If a daily pass is , after how many days does a term pass pay off? Mathematical operation(s): Solution:      Order of Operations with Whole Numbers   Use the order of operations to complete each problem. Perform one operation at a time and write the answer in its place. Show each step vertically with an equal sign on each line. There are two examples to show you the proper form:  Example A:   Example B:                         Self Reflection On a scale of 1-5, how confident are you with the material in this worksheet? Were any of the exercises confusing or frustrating? If so, identify them here and write down your plan to master those exercises.    "
+  "body": " Whole Numbers   This worksheet references topics from Chapter 1.    Worksheet: Arithmetic Practice    Practice arithmetic in a handful of different forms  Communicate your process and conclusion clearly and with appropriate units  Identify confusing symbols or operations so you know what to review      Practice with Units   Perform the indicated operation and write your answer with the proper units.                        Word Association   What operation is associated with each of the following words?   total  shared equally  double  loss  goes into  triple  reduced by  altogether    Problem Solving with Whole Numbers   For each problem, decide which mathematical operation(s) is appropriate to use and then find the answer. Show your thinking with words, symbols, and\/or pictures. Write your answer in a complete sentence.   A movie theater makes a profit on each ticket sold. How many tickets must be sold to make a profit of ? Mathematical operation(s): Solution:  A savings account contained . After a withdrawal of and a deposit of , how much is now in the account? Mathematical operation(s): Solution:  How many tablets should a pharmacist give a person who needs to take tablets times a day for days? Mathematical operation(s): Solution:  The cost of a student parking pass at PCC is per term if you buy it online. If a daily pass is , after how many days does a term pass pay off? Mathematical operation(s): Solution:      Order of Operations with Whole Numbers   Use the order of operations to complete each problem. Perform one operation at a time and write the answer in its place. Show each step vertically with an equal sign on each line. There are two examples to show you the proper form:  Example A:   Example B:                         Self Reflection On a scale of 1-5, how confident are you with the material in this worksheet? Were any of the exercises confusing or frustrating? If so, identify them here and write down your plan to master those topics.    "
 },
 {
   "id": "worksheet-arithmetic-practice-2",
@@ -385,7 +403,187 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "33",
   "title": "Self Reflection.",
-  "body": "Self Reflection On a scale of 1-5, how confident are you with the material in this worksheet? Were any of the exercises confusing or frustrating? If so, identify them here and write down your plan to master those exercises. "
+  "body": "Self Reflection On a scale of 1-5, how confident are you with the material in this worksheet? Were any of the exercises confusing or frustrating? If so, identify them here and write down your plan to master those topics. "
+},
+{
+  "id": "group-dice-sum-game",
+  "level": "1",
+  "url": "group-dice-sum-game.html",
+  "type": "Subsection",
+  "number": "",
+  "title": "Group Activity: Race to 1000",
+  "body": " Group Activity: Race to 1000   You will need:  Two 12-sided dice  Pencil and paper      How To Play    Determine who will play first by having each player roll two dice. The player with the highest product goes first. Re-roll to break ties.  Each player starts with 0 points.  On your turn, roll the two dice and multiply the two results together. Add the product to your points.  If you roll doubles, announce that you rolled a \"Perfect Square,\" record your points, and take another turn.  Pass the dice to your left so that play continues clockwise.  The first player to reach a total of 1000 points or greater wins!     "
+},
+{
+  "id": "ws-integers",
+  "level": "1",
+  "url": "ws-integers.html",
+  "type": "Subsection",
+  "number": "",
+  "title": "Integers and the Number Line",
+  "body": " Integers and the Number Line   This worksheet references topics from Chapter 1, as well as Chapter 10.1, Section 1: Integers on page 588.    Worksheet: Number Line Practice    Practice working with and communicating about negative numbers  Make number comparisons and identify where numbers are on the number line      Introduction to Integers   Discuss each of the following situations with your group and write about what it means for a number to be positive, negative, and zero.   Temperature Positive: Negative: Zero:  Checking account balance Positive: Negative: Zero:  Elevation Positive: Negative: Zero:  Profit for a business Positive: Negative: Zero:     Answer the following by comparing the two values.   Which is warmer: F or F?  How much warmer is F than F?      Rewrite each statement to use negative numbers instead of positive numbers, changing terms if needed to convey the same information.  We were feet below sea level. The brewing company had a loss in 2015. The temperature outside was F below zero.  The Number Line  Use the number line to solve the problems.     (Start on , move right ) (Start on , move left ) (Start on , move left )    Multiplying Integers  Write each multiplication as an equivalent addition problem. Make up a context for it. Two examples are provided.  Example A: .  I made quilts per month for months, making quilts in total.  Example B: .  I bought movie tickets for dollars each, spending a total of dollars.   Exploration What can you say about a negative number multiplied by a positive number? What can you say about a negative number multiplied by a negative number?     Comparing Integers   Think about where each pair of numbers is on the number line and circle the correct statement comparing them. Then fill in the mathematical symbol ( or ) showing the relationship.   2 and 5 is to the (left\/right) of on the number line. is (less than\/greater than) . .  3 and -3 is to the (left\/right) of on the number line. is (less than\/greater than) . .  -4 and -6 is to the (left\/right) of on the number line. is (less than\/greater than) . .  -6 and -4 is to the (left\/right) of on the number line. is (less than\/greater than) . .   Self Reflection On a scale of 1-5, how confident are you with the material in this worksheet? Were any of the exercises confusing or frustrating? If so, identify them here and write down your plan to master those topics.    "
+},
+{
+  "id": "worksheet-number-line-practice-2",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  Practice working with and communicating about negative numbers  Make number comparisons and identify where numbers are on the number line   "
+},
+{
+  "id": "worksheet-number-line-practice-3-1-3",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-3-1-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "Temperature.",
+  "body": "Temperature Positive: Negative: Zero: "
+},
+{
+  "id": "worksheet-number-line-practice-3-1-4",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-3-1-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "Checking account balance.",
+  "body": "Checking account balance Positive: Negative: Zero: "
+},
+{
+  "id": "worksheet-number-line-practice-3-1-5",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-3-1-5",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "Elevation.",
+  "body": "Elevation Positive: Negative: Zero: "
+},
+{
+  "id": "worksheet-number-line-practice-3-1-6",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-3-1-6",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "Profit for a business.",
+  "body": "Profit for a business Positive: Negative: Zero: "
+},
+{
+  "id": "worksheet-number-line-practice-3-2-2",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-3-2-2",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "Which is warmer: F or F? "
+},
+{
+  "id": "worksheet-number-line-practice-3-2-3",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-3-2-3",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "How much warmer is F than F? "
+},
+{
+  "id": "worksheet-number-line-practice-4-1",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-4-1",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": " Rewrite each statement to use negative numbers instead of positive numbers, changing terms if needed to convey the same information.  We were feet below sea level. The brewing company had a loss in 2015. The temperature outside was F below zero. "
+},
+{
+  "id": "worksheet-number-line-practice-4-2",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-4-2",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "The Number Line.",
+  "body": "The Number Line  Use the number line to solve the problems.     (Start on , move right ) (Start on , move left ) (Start on , move left ) "
+},
+{
+  "id": "worksheet-number-line-practice-5-1",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-5-1",
+  "type": "Worksheet Exercise",
+  "number": "9",
+  "title": "Multiplying Integers.",
+  "body": "Multiplying Integers  Write each multiplication as an equivalent addition problem. Make up a context for it. Two examples are provided.  Example A: .  I made quilts per month for months, making quilts in total.  Example B: .  I bought movie tickets for dollars each, spending a total of dollars.  "
+},
+{
+  "id": "worksheet-number-line-practice-5-2",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-5-2",
+  "type": "Worksheet Exercise",
+  "number": "10",
+  "title": "Exploration.",
+  "body": "Exploration What can you say about a negative number multiplied by a positive number? What can you say about a negative number multiplied by a negative number? "
+},
+{
+  "id": "worksheet-number-line-practice-6-1-3",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-6-1-3",
+  "type": "Worksheet Exercise",
+  "number": "11",
+  "title": "2 and 5.",
+  "body": "2 and 5 is to the (left\/right) of on the number line. is (less than\/greater than) . . "
+},
+{
+  "id": "worksheet-number-line-practice-6-1-4",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-6-1-4",
+  "type": "Worksheet Exercise",
+  "number": "12",
+  "title": "3 and -3.",
+  "body": "3 and -3 is to the (left\/right) of on the number line. is (less than\/greater than) . . "
+},
+{
+  "id": "worksheet-number-line-practice-6-1-5",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-6-1-5",
+  "type": "Worksheet Exercise",
+  "number": "13",
+  "title": "-4 and -6.",
+  "body": "-4 and -6 is to the (left\/right) of on the number line. is (less than\/greater than) . . "
+},
+{
+  "id": "worksheet-number-line-practice-6-1-6",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-6-1-6",
+  "type": "Worksheet Exercise",
+  "number": "14",
+  "title": "-6 and -4.",
+  "body": "-6 and -4 is to the (left\/right) of on the number line. is (less than\/greater than) . . "
+},
+{
+  "id": "worksheet-number-line-practice-6-2",
+  "level": "2",
+  "url": "ws-integers.html#worksheet-number-line-practice-6-2",
+  "type": "Worksheet Exercise",
+  "number": "15",
+  "title": "Self Reflection.",
+  "body": "Self Reflection On a scale of 1-5, how confident are you with the material in this worksheet? Were any of the exercises confusing or frustrating? If so, identify them here and write down your plan to master those topics. "
+},
+{
+  "id": "group-zero-sum-game",
+  "level": "1",
+  "url": "group-zero-sum-game.html",
+  "type": "Subsection",
+  "number": "",
+  "title": "Group Activity: Zero Sum",
+  "body": " Group Activity: Zero Sum   You will need a deck of playing cards with the jokers and any marketing cards removed and set aside.  Aces are worth 1 point, and face cards (Jacks, Queens, and Kings) are worth 10 points.  Black suits (Spades, Clubs) are worth positive points. Red suits (Hearts, Diamonds) are worth negative points.      How To Play    Choose one player to be the dealer. The dealer shuffles the deck and deals 3 cards to each player.  Play starts with the player to the dealer's left. The table score starts at 0.  On your turn, choose a card from your hand and play it onto the discard pile. Add its point value to the table score, and say the new table score out loud.  If the table score is 0, you win the pile! Collect the discard pile and set it aside, then play another card from your hand to start a new discard pile.  Draw cards to bring your hand back up to 3 cards, or until the deck is empty.  Play continues to the left (clockwise) until all cards have been played. If a player has no cards in hand on their turn, their turn is skipped.  Count up how many cards you've won. Whoever won the most cards wins the round!     "
+},
+{
+  "id": "problem-set-1",
+  "level": "1",
+  "url": "problem-set-1.html",
+  "type": "Subsection",
+  "number": "",
+  "title": "Homework: Problem Set 1",
+  "body": " Homework: Problem Set 1   In addition to the homework problems on ALEKS, complete the following problems by hand:    Book Problems    Page 89: 22-28, 37-42  Page 90: 53-61  Page 91: 71, 72, 77, 86, 88, 91, 93, 98  Page 494: 1, 2, 9-17  Page 594: 1-6, 37-40     "
 },
 {
   "id": "fractions",
